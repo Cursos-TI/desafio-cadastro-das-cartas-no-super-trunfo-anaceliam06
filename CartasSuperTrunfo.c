6ab9a96d17e77b160b3b15d9118903ce42d58a07
anaceliam06 <anaceliamarques123@gmail.com>
2025-03-03 13:12:36 +0000

    fazendo o primeiro teste da CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
  
-int main() {
-    printf("Desafio Super Trunfo - Países"\n");
+int main(){
+    printf("Desafio Super Trunfo - Países \n");
+        
+    char estado [50], codigo_da_carta [50], nome_da_cidade [50];    
+    int populacao, numero_de_pontos_turisticos;
+    float area, PIB; 
     
-        
+    printf("Estado: \n");
+    scanf("%s", &estado);
 
-        
-    int População, Número de pontos turísticos;
-    float Área, PIB;    
+    printf("Carta: \n");
+    scanf("%s", &codigo_da_carta);
+
+    printf("Cidade: \n");
+    scanf("%s", &nome_da_cidade);
+
+    printf("Polulação: \n");
+    scanf("%d", &populacao);
+
+    printf("Pontos Turísticos: \n");
+    scanf("%d", &numero_de_pontos_turisticos);
+
+    printf("Area: \n");
+    scanf("%f", &area);
+
+    printf("PIB: \n");
+    scanf ("%f", &PIB);
+
+    printf("Estado: %s \n Carta: %s \n Cidade: %s \n População: %d \n Pontos Turísticos: %d \n Area: %f \n PIB: %f \n", estado, codigo_da_carta, nome_da_cidade, populacao, numero_de_pontos_turisticos, area, PIB);
+
+    
 
     return 0;
 }
