9cdea09f440663fe1836a813e168a8b1f787953f
anaceliam06 <anaceliamarques123@gmail.com>
2025-03-05 00:29:38 +0000

    finalizando cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
  
 int main(){
-    printf("Desafio Super Trunfo - Países \n");
+    printf("Desafio Super Trunfo - Países Carta 1 \n");
         
-    char estado [50], codigo_da_carta [50], nome_da_cidade [50];    
+    char estado[50], codigo_da_carta[50], nome_da_cidade[50];    
     int populacao, numero_de_pontos_turisticos;
     float area, PIB; 
     
     printf("Estado: \n");
-    scanf("%s", &estado);
+    scanf("%s", estado);
 
     printf("Carta: \n");
-    scanf("%s", &codigo_da_carta);
+    scanf("%s", codigo_da_carta);
 
     printf("Cidade: \n");
-    scanf("%s", &nome_da_cidade);
+    scanf("%s", nome_da_cidade);
 
-    printf("Polulação: \n");
+    printf("População: \n");
     scanf("%d", &populacao);
 
     printf("Pontos Turísticos: \n");
@@ -28,9 +28,33 @@ int main(){
     printf("PIB: \n");
     scanf ("%f", &PIB);
 
-    printf("Estado: %s \n Carta: %s \n Cidade: %s \n População: %d \n Pontos Turísticos: %d \n Area: %f \n PIB: %f \n", estado, codigo_da_carta, nome_da_cidade, populacao, numero_de_pontos_turisticos, area, PIB);
+    printf("\n\nCarta 1: \nEstado: %s \n Carta: %s \n Cidade: %s \n População: %d \n Pontos Turísticos: %d \n Area: %f \n PIB: %f \n \n", estado, codigo_da_carta, nome_da_cidade, populacao, numero_de_pontos_turisticos, area, PIB);
 
-    
+    printf("Desafio Super Trunfo - Países Carta 2 \n");
+
+    printf("Estado: \n");
+    scanf("%s", estado);
+
+    printf("Carta: \n");
+    scanf("%s", codigo_da_carta);
+
+    printf("Cidade: \n");
+    scanf("%s", nome_da_cidade);
+
+    printf("População: \n");
+    scanf("%d", &populacao);
+
+    printf("Pontos Turísticos: \n");
+    scanf("%d", &numero_de_pontos_turisticos);
+
+    printf("Area: \n");
+    scanf("%f", &area);
+
+    printf("PIB: \n");
+    scanf ("%f", &PIB);
+
+    printf("\n\nCarta 2: \nEstado: %s \n Carta: %s \n Cidade: %s \n População: %d \n Pontos Turísticos: %d \n Area: %f \n PIB: %f \n", estado, codigo_da_carta, nome_da_cidade, populacao, numero_de_pontos_turisticos, area, PIB);
+
 
     return 0;
 }
